66c1ed122e06d8036d593ea2227b7ed05ab2d15f
Chris Frewin <frewin.christopher@gmail.com>
2021-02-23 16:37:37 +0100

    how to create aliases and the bash profile!

---
+++ lesson-9.sh
@@ -0,0 +1,4 @@
+# Commands covered in this lesson
+# alias - asign with an equals sign (typically need to wrap entire command with single or double quotes for it to be accepted)
+alias showdesktop='cd /Users/chris/Desktop && open . && cd -' # we can now type simply 'showdesktop' for our custom chained function to fire off!
+# source .bash_profile - sources the contents of the bash profile - will make any new aliases or functions immediately available
