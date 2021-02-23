4a4f55188c6c1d1e36a5361337a0d8036d7f14c2
Chris Frewin <frewin.christopher@gmail.com>
2021-02-23 15:41:19 +0100

    output redirection, input redirection, and piping!

---
+++ lesson-7.sh
@@ -0,0 +1,7 @@
+# Commands and syntax covered in this lesson
+# 1. > - output redirection - redirect the output of what is on the left to the right
+# 2. < - input redirection - read or take input of what is on the right to the left
+# 3. | - piping - pass on the output to the next command
+# 4. tr - translates specific characters
+# 5. tr [a-z] [A-Z] - translates all lowercase letters a through z to all uppercase letters A-Z
+
