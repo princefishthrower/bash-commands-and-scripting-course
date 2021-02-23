79e03e313d0367783e62d30c6de34608141b38e1
Chris Frewin <frewin.christopher@gmail.com>
2021-02-23 11:56:45 +0100

    lesson 3 - introduction bash commands!

---
+++ lesson-3.sh
@@ -0,0 +1,9 @@
+# comments comments comments!
+
+# commands covered in this lesson:
+# 1. pwd - stands for 'print working directory', show your current location on the system in the shell
+# 2. ls - stands for 'list', lists files to standard output
+# 3. cd - change directory
+# 4. open - opens file according to system defaults (for this shell file, it turns out it was XCode on mac)
+# 5. clear - clear the current visible part of the window in terminal (but doesn't actually delete any output history)
+# 6. cat - stands for 'conCATenate', prints the contents of a file to standard output
