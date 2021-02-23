ea5af3e5fa495bd09205278c9b95e1edbf8f8508
Chris Frewin <frewin.christopher@gmail.com>
2021-02-23 16:04:25 +0100

    learning how to chain commands when there is no input or output data!

---
+++ lesson-8.sh
@@ -0,0 +1,8 @@
+# Commands and syntax covered in this lesson
+# ; - seperates commands - though not in a fail-safe way, bash will continue reading
+# && - logical AND - will execute commands in a chain in series, only if each subsequent command is successful
+# & - will execute all commands in chain in parallel
+# open . - opens current directory
+# cd - - move back to previous directory
+# cd .. move back a folder
+# cd /Users/chris/Desktop && open . && cd -   this will open the desktop, and move your shell location back to where you were when you issued the command
