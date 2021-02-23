951b92e8224cbdae5ac07cb454a401d1b11085c5
Chris Frewin <frewin.christopher@gmail.com>
2021-02-23 13:19:10 +0100

    using sed with a flag!

---
+++ lesson-6.sh
@@ -0,0 +1,4 @@
+# Commands and command combinations covered in this lesson
+# 1. sed - stands for 'stream editor'
+# 2. sed 's/blue/pink/g' file1.txt - substitutes blue to pink
+# 3. sed -i ".backup" 's/blue/pink/g' file1.txt - will substitute blue to pink in-place on file1.txt, and also leve a file1.txt.backup with the original contents
