9d591d093c58bdb67a7a660e8d6b2aeef9c01734
Chris Frewin <frewin.christopher@gmail.com>
2021-02-23 12:21:52 +0100

    manipulation and modifications commands!

---
+++ lesson-4.sh
@@ -0,0 +1,8 @@
+# Commands covered in this lesson:
+# 1. cp - stands for copy - copy target file into destination file or folder
+# 2. mv - stands for move - moves target into destination file or folder
+# 3. touch - will create a file with the name passed target file (if it doesn't exist yet)
+# 4. mkdir - stands for make directory, creates a directory of the name passed (if it doesn't exist yet)
+# 5. nano - edit a file in directly in the shell
+# 6. rm - remove target file, PERMANANT AND NON-REVERSIBLE!
+
