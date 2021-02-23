8596f01edd5ad2e3e612d1528910e7603e6a54d3
Chris Frewin <frewin.christopher@gmail.com>
2021-02-23 12:50:23 +0100

    learning about the manual and flags!

---
+++ lesson-5.sh
@@ -0,0 +1,5 @@
+# Commands and flags covered in this lesson
+# man - shows the manual page for the target comand
+# ls -a - list all files including hidden ones
+# ls -l - list all files in a verticle fashion with some other info
+# cp -R - copy entire folder including its contents
