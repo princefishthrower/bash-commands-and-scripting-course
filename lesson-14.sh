f06d162ae1ec303b1ee340c80582c410374894b4
Chris Frewin <frewin.christopher@gmail.com>
2021-03-11 10:52:11 +0100

    learning about tree, free, memory_pressure, and ifconfig!

---
+++ lesson-14.sh
@@ -0,0 +1,8 @@
+# commands covered in this lesson
+tree # create a folder and file tree of wherever you are in the terminal pass -I to ignore files by a certain pattern
+
+free # available on linux systems, shows the amount of free memory you have. pass -h to view it in a human readable form
+
+memory_pressure # the closest mac-equivalent to free
+
+ifconfig # find both your public IP address and also your IP address on your local area network (LAN)
