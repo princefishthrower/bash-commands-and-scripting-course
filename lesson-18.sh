d1f580b6c3f9f751a3310999e27c6a9cba27c1b4
Chris Frewin <frewin.christopher@gmail.com>
2021-03-11 19:56:39 +0100

    learning all about environment variables in bash

---
+++ lesson-18.sh
@@ -0,0 +1,14 @@
+# we can't access myenv var after running this script :(
+function test() {
+    myvar=1234
+
+    echo $myvar
+}
+test
+
+# other commands 
+
+chmod # change the mode of the file. to make it executable, use chmod +x
+
+
+
