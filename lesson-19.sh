348cd2d7b3fbf1bea507fe6a813832a06bf31ad6
Chris Frewin <frewin.christopher@gmail.com>
2021-03-12 13:12:11 +0100

    learning about the which command and the --version flag

---
+++ lesson-19.sh
@@ -0,0 +1,15 @@
+# commands (and flags!) covered in this lesson
+
+which # locate a program file in the user's path, for whatever program you pass into into
+
+# ex.
+which node
+which python
+which python3
+which gatsby
+which react
+which typescript
+
+--version
+# This is a flag that is implemented (nearly always?) and will print out that tool or program's version number
+# Just a light warning /reminder that it is not a "true" standard, only a pseudo standard - but I've yet to find a program that doesn't implement it :)
